ab19fc921029123b7607e24aaab31523310ed4a9
Chris Coleman @ Rot-26 Solutions LLC <chris@rot-26.com>
2014-07-14 18:28:40 -0600

    modified info.php title block

--- www/html/info.php
+++ www/html/info.php
@@ -1,1 +1,18 @@
-<?php echo phpinfo(); ?>
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Short description for file
+ *
+ * Long description for file (if any)...
+ *
+ * PHP version 5
+ *
+ */
+
+$test = array('one','two','three','four','five');
+var_dump($test);
+
+echo phpinfo();
+
+
